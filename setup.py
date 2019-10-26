26f8c169f2e7394048641faa6bba2d367fe432a8
Frank Xu <franky.xhl@gmail.com>
2019-10-26 13:23:59 +0900

    Upgrade to 0.2.1

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["PyYAML>=5.1.1"]
+requirements = ["PyYAML>=5.1.2"]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Frank Xu",
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/frankyxhl/py_fx_lib',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
 )
